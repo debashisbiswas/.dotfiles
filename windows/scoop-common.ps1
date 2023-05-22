039120651f9abd131d350e122e2f74e666f1fa44
Debashis Biswas <debashis.biswas@asu.edu>
2023-05-21 20:43:12 -0700

    Solve volta install

--- windows/scoop-common.ps1
+++ windows/scoop-common.ps1
@@ -1,1 +1,19 @@
-scoop install bat fd file fzf ripgrep starship sudo gh neovim glazewm gcc
+scoop install `
+    bat `
+    dark `
+    fd `
+    file `
+    fzf `
+    gcc `
+    gh `
+    glazewm `
+    innounp `
+    make `
+    neovim `
+    ripgrep `
+    sed `
+    starship `
+    sudo `
+
+# Volta must be installed as admin.
+sudo scoop install volta
