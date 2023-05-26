11eacfaf49e077e9d88880a9c8ca21b1b4a0c9d3
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2023-05-26 16:29:30 -0700

    Disable unused plugins, add more plugins

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,18 @@
 return {
   { "mini.surround", enabled = false },
+  { "mini.indentscope", enabled = false },
+  { "mini.comment", enabled = false },
+  { "mini.ai", enabled = false },
+
   { "noice.nvim", enabled = false },
   { "nvim-notify", enabled = false },
+  { "dressing.nvim", enabled = false },
+
+  { "neo-tree.nvim", enabled = false },
+  { "nui.nvim", enabled = false },
+
+  { "flit.nvim", enabled = false },
+  { "leap.nvim", enabled = false },
+
+  { "catppuccin", enabled = false },
 }
