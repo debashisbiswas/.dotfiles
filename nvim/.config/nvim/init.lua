c6fe894d08a95fbbf374306055eda0fabc15efb3
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2023-07-14 11:20:33 -0700

    Shrink nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,26 +43,6 @@ require('lazy').setup({
   },
 
   {
-    'projekt0n/github-nvim-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('github-theme').setup {
-        options = {
-          transparent = true,
-          styles = {
-            -- disable italics
-            comments = 'NONE',
-            keywords = 'NONE',
-          },
-        },
-      }
-
-      vim.cmd 'colorscheme github_dark'
-    end,
-  },
-
-  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     opts = {
@@ -74,27 +54,8 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-      show_current_context = true,
-    },
-  },
-
-  { 'JoosepAlviste/nvim-ts-context-commentstring', lazy = true },
   { 'numToStr/Comment.nvim', opts = {} },
 
-  {
-    'folke/trouble.nvim',
-    opts = {
-      icons = false,
-    },
-  },
-
-  -- For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'plugins' },
 }, {})
 
