fcf098988b6fe8e95c05f7ff76f5306b775eb734
Debashis Biswas <debashis.biswas@asu.edu>
2023-07-29 21:53:12 -0700

    fix treesitter?

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -11,10 +11,12 @@ return {
 
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = 'all',
+      ensure_installed = {
+        'lua'
+      },
 
       -- Automatically install missing parsers when entering buffer
-      auto_install = false,
+      auto_install = true,
 
       indent = { enable = true },
 
