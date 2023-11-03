74fddf835a73c6f305c14698efd8dcceb20b2006
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2023-11-03 10:19:53 -0700

    wezterm on windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,17 +1,76 @@
 local wezterm = require("wezterm")
-local config = {}
 
+local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+local function disable_ligatures(config_table)
+	config_table.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+end
+
+config.color_scheme = "Catppuccin Mocha"
 config.font_size = 16
 config.window_background_opacity = 0.9
 config.force_reverse_video_cursor = true
-config.color_scheme = "Catppuccin Mocha"
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.font = wezterm.font({ family = "Iosevka", weight = "DemiBold" })
+config.automatically_reload_config = true
+
+disable_ligatures(config)
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "pwsh" }
+	config.cell_width = 0.9
+	config.font_size = 14
+    config.window_background_opacity = 0.95
+
+	-- tmux-like
+	config.leader = { mods = "CTRL", key = "s" }
+	config.keys = {
+		{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+
+		{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+		{
+			key = "\\",
+			mods = "LEADER",
+			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+		},
+		{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+		{ key = "v", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+
+		{ key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
+		{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+
+		{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+
+		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+
+		{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+		{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 7 } }) },
+		{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+		{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+
+		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+
+		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+
+		{ key = "n", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "p", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = -1 }) },
+	}
+end
 
 return config
