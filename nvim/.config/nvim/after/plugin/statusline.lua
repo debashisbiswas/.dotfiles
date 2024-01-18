6566364ddff109e73bd0dde8b48f744c9c42dab2
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-01-18 13:26:18 -0700

    lualine alternate file

--- nvim/.config/nvim/after/plugin/statusline.lua
+++ nvim/.config/nvim/after/plugin/statusline.lua
@@ -1,13 +1,25 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    component_separators = { left = '|', right = '|' },
-    section_separators = { left = '', right = '' },
+    component_separators = '',
+    section_separators = '',
   },
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = {
+      { 'filename', separator = {} },
+      function()
+        local result = ""
+        local current = vim.fn.expand '%:t'
+        local alternate = vim.fn.expand '#:t'
+        if alternate ~= '' and alternate ~= current then
+          result = '[' .. '#' .. alternate .. ']'
+        end
+
+        return result
+      end,
+    },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
