8e3bc568a464a814781dd8d1b85d5772dee6875f
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-06-04 22:49:14 -0700

    NixOS is the best gaming distro

--- nixos/hosts/lumine/default.nix
+++ nixos/hosts/lumine/default.nix
@@ -7,5 +7,12 @@
   ];
 
   networking.hostName = "lumine";
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   system.stateVersion = "23.11";
 }
