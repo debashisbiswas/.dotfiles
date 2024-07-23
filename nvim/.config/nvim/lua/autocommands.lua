8079d06ae0b8079df4fb5fbb21a8d2ed45eab0ed
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-07-23 09:24:43 -0700

    Remove whitespace autocommand

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -10,20 +10,20 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-  group = config_group,
-  pattern = '*',
-  -- save/restore search term, remove trailing whitespace, remove trailing empty lines
-  -- TODO: this should save your place with a mark or something
-  callback = function()
-    -- Trim trailing whitespace
-    local curpos = vim.api.nvim_win_get_cursor(0)
-    vim.cmd [[keeppatterns %s/\s\+$//e]]
-    vim.api.nvim_win_set_cursor(0, curpos)
-
-    -- Trim blank lines at end of buffer
-    local n_lines = vim.api.nvim_buf_line_count(0)
-    local last_nonblank = vim.fn.prevnonblank(n_lines)
-    if last_nonblank < n_lines then vim.api.nvim_buf_set_lines(0, last_nonblank, n_lines, true, {}) end
-  end,
-})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   group = config_group,
+--   pattern = '*',
+--   -- save/restore search term, remove trailing whitespace, remove trailing empty lines
+--   -- TODO: this should save your place with a mark or something
+--   callback = function()
+--     -- Trim trailing whitespace
+--     local curpos = vim.api.nvim_win_get_cursor(0)
+--     vim.cmd [[keeppatterns %s/\s\+$//e]]
+--     vim.api.nvim_win_set_cursor(0, curpos)
+--
+--     -- Trim blank lines at end of buffer
+--     local n_lines = vim.api.nvim_buf_line_count(0)
+--     local last_nonblank = vim.fn.prevnonblank(n_lines)
+--     if last_nonblank < n_lines then vim.api.nvim_buf_set_lines(0, last_nonblank, n_lines, true, {}) end
+--   end,
+-- })
