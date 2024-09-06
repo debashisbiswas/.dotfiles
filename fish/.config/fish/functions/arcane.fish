7abcad10ea31b1bb9b46d5490f1e49d6c283412e
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-09-06 14:35:11 -0700

    update arcane function

--- fish/.config/fish/functions/arcane.fish
+++ fish/.config/fish/functions/arcane.fish
@@ -1,11 +1,14 @@
 function arcane
-    set -l selected (find "$HOME/dev" -mindepth 1 -maxdepth 1 -type d | fzf)
+    set -l directories (find "$HOME/dev" -mindepth 1 -maxdepth 1 -type d)
+    set -la directories "$HOME/dev/DBTOOLS/eBug_Standalone/gui/eBug_sa"
+
+    set -l selected (string split ' ' $directories | fzf)
 
     if test -n "$selected"
         set -l session_name (string replace -a . _ (path basename $selected))
 
         if not tmux has-session -t=$session_name 2>/dev/null
-            tmux new-session -d -s $session_name -c $selected -n "editor" "nvim +Oil"
+            tmux new-session -d -s $session_name -c $selected -n "editor" "nvim ."
             tmux new-window -t $session_name -c $selected -n "terminal"
         end
         
