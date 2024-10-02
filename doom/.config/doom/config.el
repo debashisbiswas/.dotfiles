f6767e09d828c21dcde9fe87ba7ca94693fd72b0
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-10-01 22:27:25 -0700

    doom tokyo night, auto-fill markdown

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'ef-dark)
+(setq doom-theme 'doom-tokyo-night)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -81,3 +81,5 @@
 
 (add-hook 'heex-mode-hook #'tree-sitter-hl-mode)
 (add-hook 'heex-mode-hook (lambda() (tree-sitter-load 'heex)))
+
+(add-hook 'markdown-mode-hook #'auto-fill-mode)
