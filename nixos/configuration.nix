0bc3f13c269f7c2c7d71d0c352884a5ee2f9c400
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-10-08 13:09:22 -0700

    yeah what are you going to do about it

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -206,6 +206,7 @@
       alacritty
       brave
       calibre
+      vscode
       discord
       firefox
       foliate
@@ -308,10 +309,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   programs = {
     fish.enable = true;
