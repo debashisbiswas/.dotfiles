f9efb1254529bf179f25c323db6a2105bfa9bbc6
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-12-06 23:47:26 -0700

    initial hyprland config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -265,6 +265,16 @@
       slurp
       wl-clipboard
       wlsunset
+
+      # hyprland
+      kitty
+      waybar
+
+      libnotify
+      mako
+      dunst
+
+      swww
     ];
   };
 
@@ -341,6 +351,21 @@
       # Add any missing dynamic libraries for unpackaged programs
       # here, NOT in environment.systemPackages
     ];
+  };
+
+  # hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   # Disable the firewall altogether.
