75d528e6b58d3e26f1a7bdc4a79c166aeb22207e
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-12-20 17:39:58 -0700

    keymaps for executing lua live

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -11,7 +11,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setqflist, { desc = 'Open diagnostics list' })
-vim.keymap.set('n', '<leader>so', '<Cmd>source<CR>', { desc = 'Source current file' })
 
 vim.keymap.set('n', '<leader>bd', '<Cmd>bdelete<CR>', { desc = 'Delete buffer' })
 vim.keymap.set('n', '<leader>bk', '<Cmd>bdelete<CR>', { desc = 'Kill buffer' })
@@ -26,3 +25,7 @@ vim.keymap.set('n', '<leader>a,', '<Cmd>normal A,<CR>', { desc = 'line append co
 vim.keymap.set('n', '<leader>a;', '<Cmd>normal A;<CR>', { desc = 'line append semicolon' })
 vim.keymap.set('x', '<leader>a,', ':normal A,<CR>', { desc = 'visual line append comma', silent = true })
 vim.keymap.set('x', '<leader>a;', ':normal A;<CR>', { desc = 'visual line append semicolon', silent = true })
+
+vim.keymap.set('n', '<leader>so', '<Cmd>source<CR>', { desc = 'Source current file', silent = true })
+vim.keymap.set('n', '<leader>x', '<cmd>.lua<CR>', { desc = 'source line', silent = true })
+vim.keymap.set('v', '<leader>x', ':lua<CR>', { desc = 'source selection', silent = true })
