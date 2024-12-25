63c6d37688d7d9ab43baf8482aad8bcb05eb18da
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2024-12-24 18:54:36 -0700

    update oil config

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,10 @@
 return {
   'stevearc/oil.nvim',
+  lazy = false, -- need available from the CLI
   opts = {
-    view_options = { show_hidden = true }
+    view_options = { show_hidden = true },
   },
   keys = {
-    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' }
-  }
+    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
+  },
 }
