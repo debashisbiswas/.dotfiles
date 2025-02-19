f1ff567f5a1cf99e34ba39464a6b0b2e5cb0481c
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-02-19 15:18:18 -0700

    disable supermaven at work

--- nvim/.config/nvim/lua/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -1,10 +1,12 @@
 return {
   'supermaven-inc/supermaven-nvim',
+  enabled = not os.getenv 'IS_WORK_MACHINE',
   lazy = false,
   opts = {
     keymaps = {
       accept_suggestion = '<M-l>',
       accept_word = '<M-j>',
     },
+    ignore_filetypes = { 'markdown' },
   },
 }
