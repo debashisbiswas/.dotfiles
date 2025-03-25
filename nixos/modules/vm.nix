f2ff9bd4934654d796fb062eeaea81e6fa16fce8
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-03-25 13:21:32 -0700

    working vm

--- nixos/modules/vm.nix
+++ nixos/modules/vm.nix
@@ -1,7 +1,21 @@
 { pkgs, ... }:
 
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+
+        ovmf = {
+          enable = true;
+          packages = [ pkgs.OVMFFull.fd ];
+        };
+      };
+    };
+  };
+
   users.users.violet.extraGroups = [ "libvirtd" "kvm" ];
   environment.systemPackages = with pkgs; [
     virt-manager
