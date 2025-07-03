22d1be3044fc3e609d5a383794f90b7ccc7c8fb3
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-07-03 15:54:50 -0700

    iosevka-bin variants

--- nixos/shared/fonts.nix
+++ nixos/shared/fonts.nix
@@ -8,7 +8,9 @@
     fira
     fira-code
     ibm-plex
-    iosevka
+    iosevka # why can't emacs find the font when I use iosevka-bin?
+    (iosevka-bin.override { variant = "Aile"; })
+    (iosevka-bin.override { variant = "Etoile"; })
     libertine
     noto-fonts
     noto-fonts-cjk-sans
@@ -20,4 +22,3 @@
     work-sans
   ];
 }
-
