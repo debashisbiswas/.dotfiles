8d3b20815c7bd19d6189da897fecb93f6933288c
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-08-27 12:01:55 -0700

    update flake for opencode

--- nixos/shared/ai.nix
+++ nixos/shared/ai.nix
@@ -1,30 +1,8 @@
 { pkgs, inputs, ... }:
 
-let
-  opencode-latest =
-    inputs.nixpkgs-master.legacyPackages.${pkgs.system}.opencode.overrideAttrs
-      (oldAttrs: {
-        version = "0.5.10";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "sst";
-          repo = "opencode";
-          tag = "v0.5.10";
-          hash = "sha256-dodzf+WiSaxPczIwuFqNd/TiyEB2iJaR90ZGuQ9zk+Q=";
-        };
-
-        tui = oldAttrs.tui.overrideAttrs (tuiAttrs: {
-          vendorHash = "sha256-acDXCL7ZQYW5LnEqbMgDwpTbSgtf4wXnMMVtQI1Dv9s=";
-        });
-
-        node_modules = oldAttrs.node_modules.overrideAttrs (nodeAttrs: {
-          outputHash = "sha256-hznCg/7c9uNV7NXTkb6wtn3EhJDkGI7yZmSIA2SqX7g=";
-        });
-      });
-in
 {
   home.packages = with pkgs; [
     claude-code
-    opencode-latest
+    inputs.nixpkgs-master.legacyPackages.${pkgs.system}.opencode
   ];
 }
