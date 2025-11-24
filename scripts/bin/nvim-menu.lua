e83c133420e16b37ffc40e97195489af73fe5bb8
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-11-24 15:02:33 -0700

    clean up 2

--- scripts/bin/nvim-menu.lua
+++ scripts/bin/nvim-menu.lua
@@ -1,67 +1,60 @@
-local function quit()
-	vim.cmd("qa!")
-end
-
-local function fn_run_then_exit(func)
-	return function()
-		func()
-		quit()
-	end
-end
-
-local function append_lines(lines)
-	vim.api.nvim_buf_set_lines(0, -1, -1, true, lines)
-end
-
 local shortcuts = {
 	{
 		key = "b",
 		text = "(b)rowser",
-		action = fn_run_then_exit(function()
+		action = function()
 			vim.system({ "brave" })
-		end),
+		end,
 	},
 	{
 		key = "i",
 		text = "(i)deaflow",
-		action = fn_run_then_exit(function()
+		action = function()
 			vim.system({ "xdg-open", "https://ideaflow.app" })
-		end),
+		end,
 	},
 	{
 		key = "o",
 		text = "(o)bsidian",
-		action = fn_run_then_exit(function()
+		action = function()
 			local focus_result = vim.system({ "hyprctl", "dispatch", "focuswindow", "class:obsidian" }):wait()
 			local focus_success = focus_result.stdout:gsub("\n", "") == "ok"
 
 			if not focus_success then
 				vim.system({ "obsidian" })
 			end
-		end),
+		end,
 	},
 	{
 		key = "c",
 		text = "(c)laude",
-		action = fn_run_then_exit(function()
+		action = function()
 			vim.system({ "xdg-open", "https://claude.ai" })
-		end),
+		end,
 	},
 }
 
-for _, shortcut in ipairs(shortcuts) do
-	vim.keymap.set("n", shortcut.key, shortcut.action)
+local function quit()
+	vim.cmd("qa!")
 end
 
-local shortcuts_text = vim.iter(shortcuts)
+for _, shortcut in ipairs(shortcuts) do
+	vim.keymap.set("n", shortcut.key, function()
+		shortcut.action()
+		quit()
+	end)
+end
+
+local buffer_text = vim.iter(shortcuts)
 	:map(function(v)
 		return v.text
 	end)
 	:totable()
 
-vim.api.nvim_buf_set_lines(0, 0, -1, true, shortcuts_text)
+table.insert(buffer_text, "---")
+table.insert(buffer_text, "(esc) to (q)uit")
 
-append_lines({ "(esc) to (q)uit" })
+vim.api.nvim_buf_set_lines(0, 0, -1, true, buffer_text)
 
 vim.keymap.set("n", "<esc>", quit)
 vim.keymap.set("n", "q", quit)
@@ -73,4 +66,5 @@ vim.o.laststatus = 0
 vim.o.number = false
 vim.o.relativenumber = false
 vim.o.cursorline = true
-vim.bo.modifiable = false
+vim.o.showmode = false
+vim.bo.readonly = false
