4a617007225f3bbe40f56740a50e032c9b3a16dd
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2026-01-23 11:12:26 -0700

    associate pl/sql filetypes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -75,6 +75,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.highlight.on_yank { timeout = 100 } end,
 })
 
+vim.filetype.add {
+  extension = {
+    fnc = 'plsql',
+    vw = 'plsql',
+  },
+}
+
 require('mini.deps').setup()
 
 ------------------------------------------------------------
