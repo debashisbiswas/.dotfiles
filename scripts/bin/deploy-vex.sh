f7a48f06e717eafb443b421d64006d0feb12071c
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2026-03-21 12:52:03 -0700

    improve vex deploy script

--- scripts/bin/deploy-vex.sh
+++ scripts/bin/deploy-vex.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 nix run nixpkgs#nixos-rebuild -- switch \
   --flake "$DOTFILES/nixos#vexahlia" \
-  --target-host violet@vexahlia \
-  --build-host violet@vexahlia \
-  --ask-sudo-password
+  --target-host root@vexahlia \
+  --build-host root@vexahlia \
+  --no-reexec
